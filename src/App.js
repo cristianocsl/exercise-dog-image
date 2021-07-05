83dfb96281f0811a63876dc05ed9ae83d2b92e8f
cristianocsl <cslcristiano@gmail.com>
2021-07-05 13:45:53 -0300

    salvando no localstorage e dando nome aos doguinhos

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
+    console.log('Estou no constructor');
 
     this.state = {
       link: '',
+      name: '',
       loading: true,
     };
 
     this.fetchDogImages = this.fetchDogImages.bind(this);
     this.renderPhoto = this.renderPhoto.bind(this);
-    console.log('Estou no constructor');
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -25,6 +28,26 @@ class App extends Component {
       return false;
     }
     return true;
+  }
+
+  componentDidUpdate() {
+    console.log('atualizou');
+    const { link } = this.state;
+    localStorage.setItem('link', link);
+    const dog = link.split('/')[4];
+    // alert(`Raça: ${dog}`); // o alerta dispara toda vez que digita uma letra
+  }
+
+  handleChange(e) {
+    this.setState({
+      name: e.target.value,
+    });
+  }
+
+  handleSubmit(event) {
+    const { name } = this.state;
+    alert(`O nome dado foi ${name}`);
+    event.preventDefault();
   }
 
   async fetchDogImages() {
@@ -49,14 +72,28 @@ class App extends Component {
   }
 
   render() {
-    const loadingElement = <span>Loading...</span>;
-    const { renderPhoto, fetchDogImages } = this;
-    const { loading } = this.state;
-
     console.log('renderizou');
+    console.log('-----------------');
+    const loadingElement = <span>Loading...</span>;
+    const { renderPhoto, fetchDogImages, handleChange, handleSubmit } = this;
+    const { loading, name } = this.state;
+
     return (
       <div>
         <button type="button" onClick={ () => fetchDogImages() }> New Dog </button>
+        <form onSubmit={ handleSubmit }>
+          <label htmlFor="dog-name">
+            Nome do seu dog =&gt;
+            <input
+              id="dog-name"
+              type="text"
+              value={ name }
+              placeholder="Dê um nome a este dog!"
+              onChange={ handleChange }
+            />
+          </label>
+          <input type="submit" value="enviar" />
+        </form>
         <div>
           { loading ? loadingElement : renderPhoto() }
         </div>
