f1c30d3026805ce7ebf81d60640f6d379443d426
cristianocsl <cslcristiano@gmail.com>
2021-07-05 00:57:26 -0300

    Exercicio 1:

--- src/App.js
+++ src/App.js
@@ -1,30 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      link: '',
+      loading: true,
+    };
+
+    this.fetchDogImages = this.fetchDogImages.bind(this);
+    this.renderPhoto = this.renderPhoto.bind(this);
+    console.log('Estou no constructor');
+  }
+
+  componentDidMount() {
+    console.log('montou');
+    this.fetchDogImages();
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.link.includes('terrier')) {
+      return false;
+    }
+    return true;
+  }
+
+  async fetchDogImages() {
+    this.setState(
+      { loading: true },
+      async () => {
+        const response = await fetch('https://dog.ceo/api/breeds/image/random');
+        const result = await response.json();
+        this.setState({
+          loading: false,
+          link: result.message,
+        });
+      },
+    );
+  }
+
+  renderPhoto() {
+    const { link } = this.state;
+    return (
+      <img src={ link } alt="" />
+    );
+  }
+
+  render() {
+    const loadingElement = <span>Loading...</span>;
+    const { renderPhoto, fetchDogImages } = this;
+    const { loading } = this.state;
+
+    console.log('renderizou');
+    return (
+      <div>
+        <button type="button" onClick={ () => fetchDogImages() }> New Dog </button>
+        <div>
+          { loading ? loadingElement : renderPhoto() }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
